055357fd67ac77f8c398b19e9beffe3e54518e00
Ognjen Manojlovic <ogisha@MacBookPro.local>
2026-01-27 00:34:46 +0100

    fix: align docker settings env vars with DB_* and SECRET_KEY

--- truck_signs_designs/settings/test_docker.py
+++ truck_signs_designs/settings/test_docker.py
@@ -1,40 +1,48 @@
 import environ
 from .base import *
 
-DEBUG = True
+env = environ.Env(
+    DEBUG=(bool, False),
+)
 
-env = environ.Env()
-# reading env file
+# Try to read a local .env inside the container
 environ.Env.read_env()
 
-SECRET_KEY = env("DOCKER_SECRET_KEY", default=env("SECRET_KEY"))
-DEBUG = True
+# Core
+DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env("SECRET_KEY")
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# Hosts / CORS
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=["0.0.0.0", "localhost", "127.0.0.1"],
+)
 
+CORS_ALLOWED_ORIGINS = env.list(
+    "CORS_ALLOWED_ORIGINS",
+    default=["http://localhost:3000"],
+)
 
-
+# Database (Postgres)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DOCKER_DB_NAME'),
-        'USER': env('DOCKER_DB_USER'),
-        'PASSWORD': env('DOCKER_DB_PASSWORD'),
-        'HOST': env('DOCKER_DB_HOST'),
-        'PORT': env('DOCKER_DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST", default="db"),
+        "PORT": env("DB_PORT", default="5432"),
     }
 }
 
-STRIPE_PUBLISHABLE_KEY=env("DOCKER_STRIPE_PUBLISHABLE_KEY")
-STRIPE_SECRET_KEY=env("DOCKER_STRIPE_SECRET_KEY")
+# Optional integrations (only required if you actually use them)
+# If you don't have these env vars set, it won't crash.
+STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY", default="")
+STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY", default="")
 
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("DOCKER_EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("DOCKER_EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
